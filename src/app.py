27562bbd3cf30882118804704293093addd44875
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2026-01-03 10:17:37 -0500

    Remove streamlit python files

--- src/app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-home_page = st.Page("home_page.py", title="BioComp", icon="🌳")
-about_page = st.Page("about_page.py", title="About")
-usage_page = st.Page("usage_page.py", title="Usage")
-
-pg = st.navigation([home_page, about_page, usage_page])
-pg.run()
